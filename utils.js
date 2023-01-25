3e2cd558f1f46c68e88b34a4951c72070ec0fa41
Federico Marazzani <marazzani.dev@gmail.com>
2023-01-25 16:08:52 +0100

    update jwt claims

--- utils.js
+++ utils.js
@@ -1,8 +1,6 @@
 import * as jose from '/node_modules/jose';
 
-const secret = new TextEncoder().encode(
-  'cc7e0d44fd473002f1c42167459001140ec6389b7353f8088f4d9a95f2f596f2',
-);
+const secret = new TextEncoder().encode(import.meta.env.VITE_JWT_EXAMPLE_KEY);
 
 const alg = 'HS256';
 
@@ -10,10 +8,6 @@ export async function createJWT(payload) {
   const jwt = await new jose.SignJWT(payload)
     .setProtectedHeader({ alg })
     .setIssuedAt()
-    .setIssuer('urn:example:issuer')
-    .setAudience('urn:example:audience')
-    .setExpirationTime('2h')
-    .setSubject('user:id:test')
     .sign(secret)
 
   return jwt
