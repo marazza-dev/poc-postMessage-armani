9e47a6bf0f46548b9d6b002a223d954b5c145c6c
Federico Marazzani <marazzani.dev@gmail.com>
2023-01-26 07:49:25 +0100

    update iframe src to local dev env

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { createJWT } from "./utils";
 
 (async function() {
-  const iframeSrc = "https://armani-shop-staging.tailoor.com";
+  const iframeSrc = "https://armani-shop-localhost.tailoor.com:3020";
   const dialog = document.querySelector('dialog');
   dialog.returnValue = 'unauth';
   const dialogButton = document.querySelector('#dialog-btn');
